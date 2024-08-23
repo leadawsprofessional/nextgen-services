4fd1d74b946ddfda4c919c3f79cbc837c5b08da6
theneuralcraftsman <ujjwalksingh.304@gmail.com>
2024-08-23 19:34:31 +0530

    Linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,9 @@
 import { goldman, spaceGrotesk } from "@/config/font";
 import React from "react";
-import { useEffect, useState } from 'react';
-// import Router from 'next/router';
+import { FaLinkedin } from "react-icons/fa"; // Import LinkedIn icon
 import Link from "next/link";
 
-
-const Footer = ({about}) => {
-  
-
-  const handleHomeClick = () => {
-    window.location.href = '/';
-  };
-
+const Footer = ({ about }) => {
   return (
     <div className="bg-[#363537] rounded-t-[4rem] p-8 flex flex-col justify-center items-center">
       <div
@@ -21,41 +13,50 @@ const Footer = ({about}) => {
         <a href="/" className={goldman.className + " lg:text-2xl ml-3"}>
           NEXGEN SERVICES
         </a>
-          
-          {!about ? (
-              // If Home Page
-              <div className={spaceGrotesk.className + " pr-6 md:block hidden md:text-xl"}>
-                <Link href="/about-us" className="p-3">
-                  About Us
-                </Link>
-                <Link href="/#services" className="p-3">
-                  Services
-                </Link>
-                <Link href="/#packages" className="p-3">
-                  Packages
-                </Link>
-                <Link href="/#faq" className="p-3">
-                  FAQs
-                </Link>
-                </div>
-              ) : (
-              // Any other page
-              <div className={spaceGrotesk.className + " pr-6 md:block hidden md:text-xl"}>
-                <Link href="/" className="p-3">
-                  Home
-                </Link>
-                </div>
-        
 
-            )}
-        
+        {!about ? (
+          // If Home Page
+          <div
+            className={spaceGrotesk.className + " pr-6 md:block hidden md:text-xl"}
+          >
+            <Link href="/about-us" className="p-3">
+              About Us
+            </Link>
+            <Link href="/#services" className="p-3">
+              Services
+            </Link>
+            <Link href="/#packages" className="p-3">
+              Packages
+            </Link>
+            <Link href="/#faq" className="p-3">
+              FAQs
+            </Link>
+          </div>
+        ) : (
+          // Any other page
+          <div
+            className={spaceGrotesk.className + " pr-6 md:block hidden md:text-xl"}
+          >
+            <Link href="/" className="p-3">
+              Home
+            </Link>
+          </div>
+        )}
       </div>
+
       <div className="grid lg:grid-cols-2 border-b-2 w-full pb-4 mt-4">
         <div className="flex flex-col py-4 space-y-2">
-          <div className="bg-[#B9FF66] p-px rounded-lg w-36 px-5">
-            Contact Us:
-          </div>
+          <div className="bg-[#B9FF66] p-px rounded-lg w-36 px-5">Contact Us:</div>
           <div className="text-white">Email: info@targetgreencard.com</div>
+          <a
+            href="https://www.linkedin.com/company/eb1-next-level-a-nexgen-services-initiative/?viewAsMember=true"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white flex items-center space-x-2 pt-2"
+          >
+            <FaLinkedin size={28} />
+            <span>LinkedIn</span>
+          </a>
         </div>
         <div className="pt-4"></div>
       </div>
